eeac0ee93d6465c49242c1f5a08e6bccf90be073
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-12 17:06:49 -0300

    Agregada validaciones para el boton submit y nuevo type

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,37 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { Activity } from "../types"
 import { categories } from "../data/categories"
 
 export default function Form() {
 
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         category: 1,
         name: "",
         calories: 0
     })
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ["category", "calories"].includes(e.target.id)
         setActivity({
             ...activity,
-            [e.target.id]: e.target.value
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
         })
+    }
+
+    const isValidActivity = () => {
+        const {name, calories} = activity
+        return name.trim() !== "" && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        
     }
 
     return (
         <form
             className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
         >
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoria:</label>
@@ -65,8 +78,9 @@ export default function Form() {
 
             <input 
                 type="submit" 
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-                value="Guardar comida o guardar ejercicio"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+                value={activity.category === 1 ? "Guardar comida" : "Guardar ejercicio" }
+                disabled={!isValidActivity()}
                 />
         </form>
     )
