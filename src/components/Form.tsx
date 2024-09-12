3ef0103c26165663fd5a8de016e4151eca7abc14
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-11 21:24:54 -0300

    Evento para tomar valor de los input y select del formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,46 +1,73 @@
+import { ChangeEvent, useState } from "react"
 import { categories } from "../data/categories"
 
 export default function Form() {
-  return (
-    <form
-        className="space-y-5 bg-white shadow p-10 rounded-lg"
-    >
-        <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="category" className="font-bold">Categoria:</label>
-            <select
-                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-                id="category"
-            >
-                {categories.map(category => (
-                    <option
-                        key={category.id}
-                        value={category.id}
-                    >
-                        {category.name}
-                    </option>
-                ))}
-            </select>
-        </div>
 
-        <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad: </label>
-        <input 
-            id="activity"
-            type="text"
-            className="border border-slate-300 p-2 rounded-lg"
-            placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, etc"
-        />
-        </div>
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: "",
+        calories: 0
+    })
 
-        <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="calories" className="font-bold">Calorias: </label>
-        <input 
-            id="calories"
-            type="number"
-            className="border border-slate-300 p-2 rounded-lg"
-            placeholder="Calorias. ej. 300 o 500"
-        />
-        </div>
-    </form>
-  )
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    return (
+        <form
+            className="space-y-5 bg-white shadow p-10 rounded-lg"
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categoria:</label>
+                <select
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    id="category"
+                    value={activity.category}
+                    onChange={handleChange}
+                >
+                    {categories.map(category => (
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="name" className="font-bold">Actividad: </label>
+            <input 
+                id="name"
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, etc"
+                value={activity.name}
+                onChange={handleChange}
+            />
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories" className="font-bold">Calorias: </label>
+            <input 
+                id="calories"
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Calorias. ej. 300 o 500"
+                value={activity.calories}
+                onChange={handleChange}
+            />
+            </div>
+
+            <input 
+                type="submit" 
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+                value="Guardar comida o guardar ejercicio"
+                />
+        </form>
+    )
 }
