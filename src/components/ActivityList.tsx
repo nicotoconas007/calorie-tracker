8a5d845f6b4def1157cef49e0be549317587b823
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-12 21:05:50 -0300

    Agregado boton para eliminar actividades y logica para guardar en LocalStorage

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 import { useMemo, Dispatch } from "react"
-import { PencilSquareIcon} from "@heroicons/react/24/outline"
+import { PencilSquareIcon, XCircleIcon} from "@heroicons/react/24/outline"
 import { ActivityActions } from "../reducers/activity-reducer"
 
 
@@ -16,13 +16,17 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
         (category : Activity["category"]) => categories.map(cat => cat.id === category ? cat.name : "")
         ,[activities])
 
+    const isEmpty = useMemo(() => activities.length === 0, [activities])
+
   return (
     <>
         <h2 className="text-4xl font-bold text-slate-600 text-center">
             Comida y Actividades
         </h2>
 
-        {activities.map( activity => (
+        {isEmpty ? <p className="text-center my-5"> No hay actividades aún </p> :
+
+        activities.map( activity => (
             <div className="px-5 py-10 bg-white mt-5 flex justify-between" key={activity.id}>
                 <div className="space-y-2 relative">
                     <p className={`absolute -top-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-cyan-300" : "bg-orange-400"}`}>
@@ -42,6 +46,13 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
                     >
                         <PencilSquareIcon 
                             className="h-8 w-8 text-gray-800"
+                        />
+                    </button>
+                    <button
+                        onClick={() => dispatch({type: "delete-activity", payload : {id: activity.id}})}
+                    >
+                        <XCircleIcon
+                            className="h-8 w-8 text-red-500"
                         />
                     </button>
                 </div>
